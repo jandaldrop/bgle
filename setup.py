65a28240a79408eb8aa647c451e838011fa06e59
Jan Daldrop <daldrop@zedat.fu-berlin.de>
2018-07-18 17:36:48 +0200

    added setup.py (v0.1)

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import numpy as np
+
+setup(
+    name="bgle",
+    version="0.1",
+    description="Simulation code for the generalized Langevin equation",
+    author="Jan Daldrop",
+    include_dirs=[np.get_include()],
+    install_requires=['numpy'],
+    packages=["bgle"]
+)
